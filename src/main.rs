de5338c7b0a50c74f3c76d5ba9122ef658b5f2a2
Sathish Kannan <sathishkannan162@gmail.com>
2023-12-31 00:25:49 +0530

    completed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter a number to get the nth fibonacci number:");
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line.");
+    let n: i32 = input.trim().parse().expect("Please enter a valid number.");
+    let fib = fibonacci(n);
+    println!("The {n}th fibonacci number is {fib}");
+}
+
+fn fibonacci(n: i32) -> i32 {
+    if n == 1 {
+        return n;
+    } else {
+        return n + fibonacci(n - 1);
+    }
 }
